0d144648c5fa39f78715f96f034c3bdc67b39b3f
Guildroom <bagas@imperialhealthtech.com>
2023-05-02 15:08:14 +0800

    change name

--- src/dbDixie/initDb.js
+++ src/dbDixie/initDb.js
@@ -2,5 +2,5 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('myDatabase');
 db.version(1).stores({
-  friends: '++id, name, email', // Primary key and indexed props
+  friends: '++id, Name, email', // Primary key and indexed props
 });
